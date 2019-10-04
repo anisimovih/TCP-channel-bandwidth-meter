8addef002c72e63a957c4487ca4b95c7d1f7a118
anisimovih <anisimov_4@mail.ru>
2019-10-04 20:26:37 +0300

    Switch the graph from test to real.

--- graph.py
+++ graph.py
@@ -1,29 +1,16 @@
 from __future__ import unicode_literals
-from PyQt5 import QtCore, QtWidgets
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-# from matplotlib.pyplot import plot
-import matplotlib.patches as patches
-import matplotlib.pyplot as plt
-import global_variables
-from scipy.interpolate import UnivariateSpline, Akima1DInterpolator, PchipInterpolator
 import numpy as np
 
-'''class MyMplCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+from scipy.interpolate import PchipInterpolator
+from PyQt5 import QtCore
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        FigureCanvas.__init__(self, fig)
-        FigureCanvas.setSizePolicy(self,
-                                   QtWidgets.QSizePolicy.Expanding,
-                                   QtWidgets.QSizePolicy.Expanding)'''
+import global_variables
 
 
 class Graph(FigureCanvas):
-    """A canvas that updates itself every second with a new plot."""
 
     normal_speeds_quantity = 0  # Общее количество нормальных значений
     normal_speeds_number = 0  # Количество уже отображенных значений
@@ -55,7 +42,7 @@ class Graph(FigureCanvas):
         self.start_setup()
         timer = QtCore.QTimer(self)
         #timer.timeout.connect(self.draw_graph)
-        timer.timeout.connect(self.test)
+        timer.timeout.connect(self.draw_graph)
         timer.start(3000)
 
     '''def draw_start_point(self):
